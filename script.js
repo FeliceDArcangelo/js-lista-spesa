43551a11b88c674a690a6581f4d597b91283314a
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-04-21 10:26:48 +0200

    correct

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
 
-
-// let button = document.querySelector('#button')
-
-// button.addEventListener('click', function (){
-//     const listArr = []
-//     let item = document.querySelector('#shop').value
-//         while (item !== 'Fine'){
-//         listArr.push(item)
-//         item = document.querySelector('#shop').value
-//     }
-//     console.log(listArr)
-// })
-
-// const button = document.querySelector('#button')
-
-// button.addEventListener('click', function(){
-//     let i = 0 
-//     let item = document.querySelector('#shop').value
-//     while (i < 100){
-//     let item = document.querySelector('#shop').value
-//     document.querySelector('.result').innerHTML += `<span>${item}</span>`
-//     i++
-// }})
-
-
-
-
-let button = document.querySelector('#button')
-
-button.addEventListener('click', function(){
-    const listArr = []
-let item = document.querySelector('#shop').value
-let result = document.querySelector('.result')
+const numArr = []
+let list = document.querySelector('.result')
 
 let i = 0
-while (item !== 'cazzo'){
-    item = document.querySelector('#shop').value
-    result.innerHTML += listArr[item]
-    i++
+while (i < numArr.length){
+    console.log(numArr[i])
+    list.innerHTML += `<li>${numArr[i]}</li>`
 }
 
+let button = document.querySelector('#button')
+const number = document.querySelector('#shop')
+
+
+button.addEventListener('click', function(){
+    const addList = number.value 
+    numArr.push(addList)
+    console.log(addList)
+    console.log(numArr)
+    list.innerHTML += `<li>${addList}</li>`
 })
